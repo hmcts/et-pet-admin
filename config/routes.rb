1058b6ec5cb8b9653ea5c856f5e7ad42251143c1
Gary Taylor <gary.taylor@hismessages.com>
2018-03-22 17:59:20 +0000

    Moved original dummy controllers into v1 namespace Added model for reference numbers

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/admin/sidekiq'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post 'new-claim' => 'claims#create'
+      post 'fgr-et-office' => 'fee_group_offices#create'
+    end
+  end
 end
 
