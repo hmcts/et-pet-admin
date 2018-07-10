97deb488fd07aa73ea87f155c54a196f153e104e
Gary Taylor <gary.taylor@hismessages.com>
2018-07-10 19:46:41 +0100

    Added link to empty acas page in case user clicks on menu instead of hovering over it

--- app/admin/admin_acas.rb
+++ app/admin/admin_acas.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register_page 'Acas' do
   content do
-    # @TODO Decide what to do here
-    "This is some home page for acas - it exists only to go with the menu item - but could always have some useful links in it"
+    a href: admin_certificate_search_url do
+      "ACAS Certificate search"
+    end
   end
 end
