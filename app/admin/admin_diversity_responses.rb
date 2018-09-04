6af5e5534a9dc553e5a6d8a9cdb0c6042dbc2c41
Gary Taylor <gary.taylor@hismessages.com>
2018-09-04 11:13:19 +0100

    Moved diversity admin models to root namespace

--- app/admin/admin_diversity_responses.rb
+++ app/admin/admin_diversity_responses.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Admin::DiversityResponse, as: 'Diversity Responses' do
+ActiveAdmin.register ::DiversityResponse, as: 'Diversity Responses' do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- app/models/admin/diversity_response.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-module Admin
-  class DiversityResponse < ApplicationRecord
-    self.table_name = :diversity_responses
-
-  end
-end
