c8cb36e5b6776b80b085f43df1ac64d0b4212860
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-09-19 12:15:46 +0100

    Add permissions for roles, matching other policies

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 
 controlled_resources = [:offices, :jobs, :acas, :acas_download_logs,
   :addresses, :atos_files, :claims, :claimants, :exports, :exported_files,
-  :representatives, :respondents, :responses, :uploaded_files, :users, :acas_check_digits,
-  :reference_number_generators]
+  :roles, :representatives, :respondents, :responses, :uploaded_files, :users,
+  :acas_check_digits, :reference_number_generators]
+
 permissions = [:create, :read, :update, :delete, :import].product(controlled_resources).map { |pair| pair.join('_') }.sort
 
 permissions.each do |p|
