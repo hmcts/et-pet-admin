525b25e29795ccb79afbbe0f655836823fb1a48a
Gary Taylor <gary.taylor@hismessages.com>
2021-02-19 12:46:50 +0000

    RST-3155 Added redirect link (to be renamed)

---
+++ app/services/admin/redirect_claim_service.rb
@@ -0,0 +1,43 @@
+module Admin
+  class RedirectClaimService
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+    attribute :claim_id
+    attribute :office_id
+    attribute :base_url
+    def self.call(claim_id, office_id, base_url: ENV.fetch('ET_API_URL'))
+      new(claim_id: claim_id, office_id: office_id, base_url: base_url).call
+    end
+
+    def valid?
+      errors.empty?
+    end
+
+    def call
+      response = HTTParty.post("#{base_url}/v2/claims/redirect_claim",
+                               headers: {
+                                   'Accept': 'application/json',
+                                   'Content-Type': 'application/json'
+                               },
+                               body: {
+                                   uuid: SecureRandom.uuid,
+                                   command: 'RedirectClaim',
+                                   data: {
+                                       claim_id: claim_id,
+                                       office_id: office_id
+                                   },
+                                   async: false
+                               }.to_json)
+      return self if response.success?
+
+      if response['errors']
+        response['errors'].each do |err|
+          errors.add err['source'].gsub(/\A\//, '').to_sym, err['detail'], error: err['code']
+        end
+      else
+        errors.add :base, :http_error, status_code: response.code.to_s
+      end
+      self
+    end
+  end
+end
