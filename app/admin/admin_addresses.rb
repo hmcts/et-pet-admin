d4065b107d05d560230b4b11f516867dfe80e9d8
Gary Taylor <gary.taylor@hismessages.com>
2021-07-06 06:30:41 +0000

    RST-3409 Fixed error when trying to edit an address

--- app/admin/admin_addresses.rb
+++ app/admin/admin_addresses.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Address, as: 'Addresses' do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :building, :street, :locality, :county, :post_code, :country
 #
 # or
 #
@@ -11,5 +11,15 @@ ActiveAdmin.register Address, as: 'Addresses' do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+form do |f|
+    f.inputs do
+      f.input :building
+      f.input :street
+      f.input :locality
+      f.input :county
+      f.input :post_code
+      f.input :country, as: :string
+    end
+    f.actions
+  end
 end
