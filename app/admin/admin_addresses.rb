d719c35726e68c74694ed20f802a0a8f28791588
Gary Taylor <gary.taylor@hismessages.com>
2021-07-06 06:34:43 +0000

    RST-3409 Fixed issue with editing addresses

--- app/admin/admin_addresses.rb
+++ app/admin/admin_addresses.rb
@@ -1,15 +1,26 @@
 ActiveAdmin.register Address, as: 'Addresses' do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
-end
+    # See permitted parameters documentation:
+    # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+    #
+    permit_params :building, :street, :locality, :county, :post_code, :country
+    #
+    # or
+    #
+    # permit_params do
+    #   permitted = [:permitted, :attributes]
+    #   permitted << :other if params[:action] == 'create' && current_user.admin?
+    #   permitted
+    # end
+    form do |f|
+        f.inputs do
+          f.input :building
+          f.input :street
+          f.input :locality
+          f.input :county
+          f.input :post_code
+          f.input :country, as: :string
+        end
+        f.actions
+      end
+    end
+    
