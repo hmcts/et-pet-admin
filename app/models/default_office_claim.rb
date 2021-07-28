740142ab18932d85cdf8ed4ac1ce51cefde1d999
garytaylor <gary.taylor@hismessages.com>
2021-07-28 10:56:10 +0100

    RST-3453 ET Admin Dashboard - Default Office 'Actioned' (#292)
    * RST-3453 ET Admin Dashboard - Default Office 'Actioned'
    
    * RST-3453 Minor text tweaks

--- app/models/default_office_claim.rb
+++ app/models/default_office_claim.rb
@@ -1,4 +1,6 @@
 class DefaultOfficeClaim < Claim
   self.inheritance_column = :none
   default_scope -> { joins(:office).where(office: { is_default: true }) }
+  scope :actioned, -> { rewhere(manually_actioned: true) }
+  scope :not_actioned, -> { rewhere(manually_actioned: false) }
 end
