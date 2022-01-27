e5975dd68b636cff909757796b4499cd7b3a3563
Gary Taylor <gary.taylor@hismessages.com>
2022-01-27 08:19:34 +0000

    RST-4218 Maintenance page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :show_maintenance_page
+
+  private
+
+  def show_maintenance_page(config = Rails.application.config)
+    return if !config.maintenance_enabled || config.maintenance_allowed_ips.include?(request.remote_ip)
+
+    render 'static_pages/maintenance', layout: false, status: :service_unavailable
+  end
 end
