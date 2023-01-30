a45e7db162f8fab1559f88ab80e35f1c1536ff1a
Gary Taylor <gary.taylor@hismessages.com>
2023-01-30 07:59:50 +0000

    Revert "(RST-4918) Upgraded to sidekiq 7 (#305)"
    This reverts commit 583b63173a6e2631e630ad89e765902ae638accc.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.export_concurrent = false
   config.asset_host = ENV['ASSET_HOST'] if ENV['ASSET_HOST'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
