85392768c981a269769e4973d0ca1524cdbc9100
Aaron Lambley <aaron.lambley@gmail.com>
2024-03-19 14:34:21 +0000

    Remove optional option

--- app/models/export.rb
+++ app/models/export.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class Export < ApplicationRecord
   self.table_name = :exports
-  belongs_to :resource, polymorphic: true, optional: true
-  belongs_to :external_system, optional: true
+  belongs_to :resource, polymorphic: true
+  belongs_to :external_system
   has_many :events, class_name: 'ExportEvent'
 
   scope :ccd, -> { joins(:external_system).where('external_systems.reference LIKE \'ccd_%\'') }
