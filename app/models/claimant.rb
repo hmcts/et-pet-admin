e940c2305eda60d8f6e113a85179649311bfa467
garytaylor <gary.taylor@hismessages.com>
2024-04-22 13:40:56 +0100

    (RST-6570) Fixed bug preventing deletion of claimants (#381)

--- app/models/claimant.rb
+++ app/models/claimant.rb
@@ -2,9 +2,14 @@
 
 class Claimant < ApplicationRecord
   self.table_name = :claimants
-  belongs_to :address
+  belongs_to :address, dependent: :destroy
+  has_many :claim_claimants, dependent: :destroy
   accepts_nested_attributes_for :address
   def name
     "#{title} #{first_name} #{last_name}"
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[address claim_claimants]
   end
 end
