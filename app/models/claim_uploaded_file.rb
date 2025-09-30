9049674eb7363e3f0b7e1fa636b08f4e03544321
Gary Taylor <gary.taylor@hismessages.com>
2025-09-30 08:47:30 +0100

    (RST-7610) Added ability for super admins to bulk delete claims

--- app/models/claim_uploaded_file.rb
+++ app/models/claim_uploaded_file.rb
@@ -1,5 +1,5 @@
 class ClaimUploadedFile < ApplicationRecord
   self.table_name = :claim_uploaded_files
   belongs_to :claim
-  belongs_to :uploaded_file
+  belongs_to :uploaded_file, dependent: :destroy
 end
